5594dfe20f1b2f010b10d9111d5dc0aef826c433
RENNES\g.deleporte <gabysouri@gmail.com>
2023-04-18 13:58:32 +0200

    menu

---
+++ BombermanLike/Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<TextMeshProUGUI> _myTexts;
+    private int _menuState;
+
+    private void Start()
+    {
+        for (int i = 0; i < _myTexts.Count; i++)
+        {
+            if (i == _menuState)
+            {
+                _myTexts[i].color = Color.yellow;
+            }
+            else
+            {
+                _myTexts[i].color = Color.white;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        //choix dans le mnu
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _menuState++;
+            if (_menuState >= _myTexts.Count)
+            {
+                _menuState = 0;
+            }
+            for (int i = 0; i < _myTexts.Count; i++)
+            {
+                if (i == _menuState)
+                {
+                    _myTexts[i].color = Color.yellow;
+                }
+                else
+                {
+                    _myTexts[i].color = Color.white;
+                }
+            }
+        }
+
+        //confirmer choix
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            switch (_menuState)
+            {
+                case 0: SceneManager.LoadScene("SceneSolo");
+                    break;
+                case 1: SceneManager.LoadScene("SceneMulti");
+                    break;
+            }
+        }
+    }
+}
